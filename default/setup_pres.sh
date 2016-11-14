50587d7c7c43b2921206824f7e0b619f416b7604
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2016-11-14 14:40:52 +0100

    More updates

---
+++ default/setup_pres.sh
@@ -0,0 +1,10 @@
+def_dir="lhcb_presentation/default"
+
+mkdir $1
+cd $1
+git init
+git submodule add ssh://git@gitlab.cern.ch:7999/hschrein/lhcb_presentation.git
+cp $def_dir/presentation_temlpate.tex "$1.tex"
+cp $def_dir/gitignore .gitignore
+echo "$1.pdf" >> .gitignore
+sed s/default/$1/g < $def_dir/gitlab_ci.yml > .gitlab-ci.yml
--- setup_pres.sh
+++
@@ -1,6 +0,0 @@
-mkdir $1
-cd $1
-git init
-git submodule add ssh://git@gitlab.cern.ch:7999/hschrein/lhcb_presentation.git
-cp lhcb_presentation/presentation_temlpate.tex "$1.tex"
-
