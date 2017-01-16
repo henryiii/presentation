8534caae09fd8aea2b1f686c7ecbf620f74562b8
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-01-16 09:05:54 -0500

    More advanced setup script

--- default/setup_pres.sh
+++ default/setup_pres.sh
@@ -1,4 +1,9 @@
+# LHCb Presentation setup
+# This script can be obtained from:
+# https://gitlab.cern.ch/hschrein/lhcb_presentation/raw/master/default/setup_pres.sh
+#
 # Source this script with an argument for the name of the presentation (folder) to make
+#
 
 def_dir="lhcb_presentation/default"
 
@@ -6,7 +11,18 @@ mkdir $1
 cd $1
 git init
 git submodule add ssh://git@gitlab.cern.ch:7999/hschrein/lhcb_presentation.git
+
 cp $def_dir/presentation_temlpate.tex "$1.tex"
+git add "$1.tex"
+
 cp $def_dir/gitignore .gitignore
+git add .gitignore
+
 echo "$1.pdf" >> .gitignore
+
 sed s/default/$1/g < $def_dir/gitlab-ci.yml > .gitlab-ci.yml
+
+git commit -m "Initial commit of lhcb-presentation"
+
+echo "Add .gitlab-ci.yml to get automatic builds on gitlab"
+echo "Add a remote and push to setup your repository"
