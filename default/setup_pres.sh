9bc225ce3028cd33e757395427db74f705a7855a
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-01-16 08:57:50 -0500

    Fix setup script

--- default/setup_pres.sh
+++ default/setup_pres.sh
@@ -7,4 +7,4 @@ git submodule add ssh://git@gitlab.cern.ch:7999/hschrein/lhcb_presentation.git
 cp $def_dir/presentation_temlpate.tex "$1.tex"
 cp $def_dir/gitignore .gitignore
 echo "$1.pdf" >> .gitignore
-sed s/default/$1/g < $def_dir/gitlab_ci.yml > .gitlab-ci.yml
+sed s/default/$1/g < $def_dir/gitlab-ci.yml > .gitlab-ci.yml
